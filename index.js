cfb2c040a0d3a4fe0f9d0451e9be04e56006f412
Jan Keith Darunday <jkcdarunday@gmail.com>
2025-04-19 02:05:32 +0800

    Add backup and restore of require cache

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const { isBare } = require('which-runtime')
 
 if (isBare) {
+  const resolvedPath = require.resolve('v8-to-istanbul')
   const originalProcess = global.process
+  const originalRequireCache = require.cache[resolvedPath]
+
+  delete require.cache[resolvedPath]
   global.process = require('bare-process')
 
   try { module.exports = require('v8-to-istanbul') }
-  finally { global.process = originalProcess }
+  finally {
+    global.process = originalProcess
+    require.cache[resolvedPath] = originalRequireCache
+  }
 } else {
   module.exports = require('v8-to-istanbul')
 }
