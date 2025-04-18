e351452cece63d1baa02885fc1ec84c3c6140338
Jan Keith Darunday <jkcdarunday@gmail.com>
2025-04-19 02:07:47 +0800

    Revert "Add backup and restore of require cache"
    This reverts commit cfb2c040a0d3a4fe0f9d0451e9be04e56006f412.

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 const { isBare } = require('which-runtime')
 
 if (isBare) {
-  const resolvedPath = require.resolve('v8-to-istanbul')
   const originalProcess = global.process
-  const originalRequireCache = require.cache[resolvedPath]
-
-  delete require.cache[resolvedPath]
   global.process = require('bare-process')
 
   try { module.exports = require('v8-to-istanbul') }
-  finally {
-    global.process = originalProcess
-    require.cache[resolvedPath] = originalRequireCache
-  }
+  finally { global.process = originalProcess }
 } else {
   module.exports = require('v8-to-istanbul')
 }
