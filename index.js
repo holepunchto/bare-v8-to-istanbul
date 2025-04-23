7410c128ada432ba7441507e852d617c3270be85
Jan Keith Darunday <jkcdarunday@gmail.com>
2025-04-24 00:23:20 +0800

    Add linter and fix linting issues

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ if (isBare) {
   const originalProcess = global.process
   global.process = require('bare-process')
 
-  try { module.exports = require('v8-to-istanbul') }
-  finally { global.process = originalProcess }
+  try { module.exports = require('v8-to-istanbul') } finally { global.process = originalProcess }
 } else {
   module.exports = require('v8-to-istanbul')
 }
